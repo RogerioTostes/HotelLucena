64852719cbc17b20bb4e50e403ad16c8f731d331
RogerioTostes <40499106+RogerioTostes@users.noreply.github.com>
2020-04-26 23:59:36 -0300

    Add project

---
+++ target/generated-sources/annotations/br/cesjf/hotellucena/model/Item_.java
@@ -0,0 +1,17 @@
+package br.cesjf.hotellucena.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-26T23:57:52")
+@StaticMetamodel(Item.class)
+public class Item_ { 
+
+    public static volatile SingularAttribute<Item, String> descricaoItem;
+    public static volatile SingularAttribute<Item, String> valorItem;
+    public static volatile SingularAttribute<Item, String> nomeItem;
+    public static volatile SingularAttribute<Item, Integer> codItem;
+    public static volatile SingularAttribute<Item, Integer> idItem;
+
+}
